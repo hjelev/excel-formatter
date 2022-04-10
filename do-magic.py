0ed4c79184a158f119613a6dd410fbbf0b5b42a4
hjelev <jeleff@gmail.com>
2022-04-10 22:26:02 +0300

    formatting

--- do-magic.py
+++ do-magic.py
@@ -1,6 +1,5 @@
 import os
 import warnings
-
 import openpyxl
 from openpyxl.utils import get_column_letter
 
@@ -64,7 +63,6 @@ def check_for_hide_colums(n, e, ws):
     for i, col in enumerate(col_range):
         if '[Attr' in str(ws['{}{}'.format(col,n)].value):
             to_hide.append(col)
-            # print(ws['{}{}'.format(col,n)].value)
     return(to_hide)
 
 
@@ -73,6 +71,7 @@ def hide_cols(to_hide, ws):
         ws.column_dimensions[col.upper()].hidden = True       
     return ws
 
+
 def check_for_hide_rows(n, m, ws):
     to_hide = []
     for row in range(1, n):
@@ -80,32 +79,35 @@ def check_for_hide_rows(n, m, ws):
             to_hide.append(row)
     return to_hide
 
+
 def hide_rows(to_hide, ws):
     for row in to_hide: 
         ws.row_dimensions[row].hidden = True     
     return ws    
 
-dir_list = os.listdir(os.path.join(os.path.dirname(os.path.realpath(__file__)), "work", ""))
 
-for filename in dir_list:
-    print("---------" + filename)
-    wb = openpyxl.load_workbook(os.path.join(os.path.dirname(os.path.realpath(__file__)), "work", filename))
-    for ws_name in wb.sheetnames:
-        ws = wb[ws_name]
-        for i, row in enumerate(ws.rows):
-            ws.column_dimensions[get_column_letter(i + 1)].width = 25
-        n = check_start_a(ws) # start of first block
-        m, e = check_start_f(ws) # m = start colum of second block; e = end colum of first block; 
-        end = check_end(ws) # end of first block
-        col = check_max_col(ws) # end colum
-        to_hide = check_for_hide_colums(n, e, ws)
-        ws = hide_cols(to_hide, ws)
-        to_hide = check_for_hide_rows(n, m, ws)
-        ws = hide_rows(to_hide, ws)    
-        set_border(ws, 'A{}:{}{}'.format(n, e, end))
-        set_border(ws, '{}1:{}{}'.format(m, col, end))
+if __name__ == '__main__':
+    dir_list = os.listdir(os.path.join(os.path.dirname(os.path.realpath(__file__)), "work", ""))
 
-        set_header(ws, 'A{}:{}{}'.format(n, e, n))
-        set_header(ws, '{}1:{}{}'.format(m, m, n - 1))
+    for filename in dir_list:
+        print("---------" + filename)
+        wb = openpyxl.load_workbook(os.path.join(os.path.dirname(os.path.realpath(__file__)), "work", filename))
+        for ws_name in wb.sheetnames:
+            ws = wb[ws_name]
+            for i, row in enumerate(ws.rows):
+                ws.column_dimensions[get_column_letter(i + 1)].width = 25
+            n = check_start_a(ws) # start of first block
+            m, e = check_start_f(ws) # m = start colum of second block; e = end colum of first block; 
+            end = check_end(ws) # end of first block
+            col = check_max_col(ws) # end colum
+            to_hide = check_for_hide_colums(n, e, ws)
+            ws = hide_cols(to_hide, ws)
+            to_hide = check_for_hide_rows(n, m, ws)
+            ws = hide_rows(to_hide, ws)    
+            set_border(ws, 'A{}:{}{}'.format(n, e, end))
+            set_border(ws, '{}1:{}{}'.format(m, col, end))
 
-    wb.save(os.path.join(os.path.dirname(os.path.realpath(__file__)), "done", filename))
+            set_header(ws, 'A{}:{}{}'.format(n, e, n))
+            set_header(ws, '{}1:{}{}'.format(m, m, n - 1))
+
+        wb.save(os.path.join(os.path.dirname(os.path.realpath(__file__)), "done", filename))
